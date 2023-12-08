81a00cd329dd46372d85020ab3308c0483a24af2
qingshanlanshan <68636377+qingshanlanshan@users.noreply.github.com>
2023-12-08 23:26:20 +0800

    skip the instruction before questions

--- work.py
+++ work.py
@@ -11,8 +11,8 @@ print("请输入uis密码")
 password = input()
 '''
 
-usrname = ''
-password = ''
+usrname = '20307130048'
+password = '498662971yY'
 
 driver = webdriver.Chrome()
 url = "http://ce.fudan.edu.cn"
@@ -33,12 +33,16 @@ submit.click()
 #driver.get("http://ce.fudan.edu.cn/q.aspx?id=9957450&sqid=b956f4a567ed4ab982a7aede07a4a6da&type=5&stepseq=45ff583eeca240959051a0bf2f11efee&targettype=2&targetcode=869a78ece4374719a3ee006f8661746b&tag=b956f4a567ed4ab982a7aede07a4a6da_5_869a78ece4374719a3ee006f8661746b_2_2019-2020-2-COMP130154_0_9957450&beginTime=2020-04-24%2015:00&endTime=2020-06-14%2023:59&previewTime=2020-03-22%2008:00&name=2020%E5%B9%B4%E6%98%A5%E5%AD%A3%E5%AD%A6%E6%9C%9F%E7%90%86%E8%AE%BA%E8%AF%BE%E8%AF%84%E6%95%99-%E8%AE%A1%E7%AE%97%E6%9C%BA%E5%8E%9F%E7%90%86")
 while 1 :
     driver.get("http://ce.fudan.edu.cn/")
-    driver.find_element(By.CLASS_NAME,'evaluateMyTask').click()
-    time.sleep(1)
-    driver.switch_to.frame("iframepage")
-    a = driver.find_element(By.CSS_SELECTOR,'#tblQuestion > tbody > tr:nth-child(1) > td.operate > a.mcopen')
-    a.click()
     time.sleep(2)
+    driver.find_element(By.CLASS_NAME,'evaluateMyTask').click()
+    time.sleep(3)
+    driver.switch_to.frame("iframepage")
+    try:
+        a = driver.find_element(By.CSS_SELECTOR,'#tblQuestion > tbody > tr:nth-child(1) > td.operate > a.mcopen')
+    except:
+        break
+    a.click()
+    time.sleep(3)
     driver.switch_to.window(driver.window_handles[1])
     # radios = driver.find_elements_by_class_name('jqTransformRadioWrapper')
     # checkboxes = driver.find_elements_by_class_name('jqTransformCheckboxWrapper')
@@ -54,12 +58,13 @@ while 1 :
     #     if cnt >= 8 : break
     questions = driver.find_elements(By.CLASS_NAME,'formstyle.subject.clearfix.jqtransformdone')
     for q in questions:
+        driver.execute_script("arguments[0].scrollIntoView();", q)
         if q.is_displayed():
             radios = q.find_elements(By.CLASS_NAME,"jqTransformRadioWrapper")
             checkboxes = q.find_elements(By.CLASS_NAME,"jqTransformCheckboxWrapper")
         if (len(radios)):
             radios[0].click()
-        else:
+        elif (len(checkboxes)):
             checkboxes[0].click()
         # 等待题目充分展开
         time.sleep(0.3)
@@ -76,7 +81,7 @@ while 1 :
             if i.get_attribute('data-id')=='ok' : 
                 print('here')
                 i.send_keys(Keys.ENTER)
-    except:
-        pass
+    except Exception as e:
+        print(e)
     driver.close()
     driver.switch_to.window(driver.window_handles[0])
